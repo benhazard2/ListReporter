d746719fd4c9319134c7e0b3713b8510284c88cf
Ben Hazard <ben_hazard2@msn.com>
2019-08-26 15:45:12 +0100

    Clean up names

--- list_reporter.py
+++ list_reporter.py
@@ -2,24 +2,24 @@ import ast
 import sys
 
 def analyse_list(input_list):
-    total = 0
-    string_list = []
-    multi_string = []
+    total_strings = 0
+    unique_strings = []
+    repeated_strings = []
     for inner_list in input_list:
         index = -1
         for string in inner_list:
             index += 1
-            total += 1
-            if string not in string_list:
-                string_list.append(string)
+            total_strings += 1
+            if string not in unique_strings:
+                unique_strings.append(string)
             else:
                 if string not in inner_list[0:index]:
-                    if string not in multi_string:
-                        multi_string.append(string)
-    result = 'Strings appearing in multiple lists: ' + str(', '.join(repr(string) for string in multi_string)) + '\nNumber of unique strings: ' + str(len(string_list)) + '\nTotal number of strings processed: ' + str(total)
+                    if string not in repeated_strings:
+                        repeated_strings.append(string)
+    result = 'Strings appearing in multiple lists: ' + str(', '.join(repr(string) for string in repeated_strings)) + '\nNumber of unique strings: ' + str(len(unique_strings)) + '\nTotal number of strings processed: ' + str(total_strings)
     return result
 
 if __name__== "__main__":
     if len(sys.argv) == 1:
-        sys.exit('Please supply a list that you would like to anaylse')
+        sys.exit('Please supply a list, in quotes, that you would like to anaylse')
     print(analyse_list(ast.literal_eval(sys.argv[1])))
--- test.py
+++ test.py
@@ -1,25 +1,19 @@
 import list_reporter
 
-def test_one():
+def test_one_list():
+    tc = [['abc', 'def', 'zyx', 'bh']]
+    assert list_reporter.analyse_list(tc) == '''Strings appearing in multiple lists: 
+Number of unique strings: 4
+Total number of strings processed: 4'''
+
+def test_two_lists_one_match():
     tc = [['g', 'gh', 'ghj', 'g'], ['j', 'ju', 'gh', 'gk', 'gn']]
     assert list_reporter.analyse_list(tc) == '''Strings appearing in multiple lists: 'gh'
 Number of unique strings: 7
 Total number of strings processed: 9'''
 
-def test_two():
-    tc = [['g', 'bh'], ['j', 'g', 'bh']]
-    assert list_reporter.analyse_list(tc) == '''Strings appearing in multiple lists: 'g', 'bh'
-Number of unique strings: 3
-Total number of strings processed: 5'''
-
-def test_three():
+def test_three_lists_multiple_matches():
     tc = [['abc', 'def', 'zyx', 'bh'], ['abc', 'abc', 'bh', 'bh', 'the'], ['the', 'abc']]
     assert list_reporter.analyse_list(tc) == '''Strings appearing in multiple lists: 'abc', 'bh', 'the'
 Number of unique strings: 5
 Total number of strings processed: 11'''
-
-def test_four():
-    tc = [['abc', 'def', 'zyx', 'bh']]
-    assert list_reporter.analyse_list(tc) == '''Strings appearing in multiple lists: 
-Number of unique strings: 4
-Total number of strings processed: 4'''
